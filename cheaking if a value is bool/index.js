5de71093a70394ea84a7db5104d6c712807600cd
Victor Kimutai <vkimutai481@email.com>
2026-07-31 06:37:21 +0300

    program to cheack if value is bool

---
+++ cheaking if a value is bool/index.js
@@ -0,0 +1,17 @@
+function booWho(bool){
+  if (typeof bool === "boolean"){
+    return (true);
+  }
+  else if([1, 2, 3]) {
+    return (false);
+  }
+}
+
+console.log(booWho([1, 2, 3]));
+
+
+
+
+
+
+
